58c8e10b5318c96556e391e15207942b3c0fc79d
Alex Usbergo <alexakdrone@gmail.com>
2019-02-21 15:01:40 +0100

    initial

---
+++ bin/YAS.framework/Headers/YAS.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+#import <YAS/yaml.h>
+#import <YAS/YASObjcExceptionHandler.h>
+
+FOUNDATION_EXPORT double YASVersionNumber;
+FOUNDATION_EXPORT const unsigned char YASVersionString[];
